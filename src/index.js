2c0268d3065c1c91cd37dda15d129963a786bc7e
vova <uladzimir.yasinski@webfriends.by>
2022-01-05 16:45:16 +0300

    feat: add reverce function logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const array = String(n).split('').reverse();
+    if(array[array.length - 1] === '-'){
+        array.pop()
+    }
+    return Number(array.join(''))
 }
